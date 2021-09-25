0cc4111b175ec64c771ec7f14bb6788a077965d8
shohruh-xasanov <pfxasanov.shoxrux@gmail.com>
2021-09-25 11:38:54 +0500

    menu

--- routes/adminMenRouter.js
+++ routes/adminMenRouter.js
@@ -3,7 +3,7 @@ const {menuCreate,menuEdit} = require('../controllers/adminMenuController')
 const {roles} = require('../middleware/auth')
 
 router.route('/menu')
-    .post(roles,menuCreate)
+    .get(menuCreate)
 
 router.route('/menu/:id')
     .get(roles,menuEdit)
