75cf4011fb3e0e8fcf0e0b5fb88b101a5bb16590
shohruh-xasanov <pfxasanov.shoxrux@gmail.com>
2021-09-29 20:59:18 +0500

    full

--- middleware/visit.js
+++
@@ -1,9 +0,0 @@
-const Visit = require('../models/statistics')
-
-exports.visits = async (req,res, next)=>{
-    const visit = new Visit({
-        message:req.method
-    })
-    await visit.save()
-    next()
-}
